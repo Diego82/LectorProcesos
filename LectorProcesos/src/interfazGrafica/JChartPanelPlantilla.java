4e625690c0a45011e4d21faa34735cc5ce0493e6
diego <djtorrespe@gmail.com>
2015-11-10 00:19:12 +0100

    reformas varias

--- LectorProcesos/src/interfazGrafica/JChartPanelPlantilla.java
+++ LectorProcesos/src/interfazGrafica/JChartPanelPlantilla.java
@@ -17,17 +17,76 @@ public class JChartPanelPlantilla {
 		this.listaAux = listaAux;
 	}
 	
-	public ChartPanel ventana(){
+	public ChartPanel ventanaCPU(){
+
+		//Con esta clase cargamos los datos que queremos reflejar 
+		DefaultPieDataset data = new DefaultPieDataset();
+		double otrosProcesos = 0;
+		int procesos1=0, procesos2=0;
+        for (int i = 0; i < listaAux.size(); i++) {
+        	if(listaAux.get(i).getUsoMemoria()>0.5){
+        		data.setValue(listaAux.get(i).getPid(),listaAux.get(i).getUsoMemoria());
+        		procesos1++;
+        	}
+        	else{
+        		otrosProcesos += listaAux.get(i).getUsoMemoria();
+        		procesos2++;
+        	}	
+		}
+        System.out.println("Numero de procesos mayor que 0.5: "+procesos1);
+        System.out.println("Numero de procesos mayor que 0.5: "+procesos2);
+        
+        data.setValue("Otros procesos", otrosProcesos);
+                
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createPieChart(
+         "Uso de la CPU", 
+         data, 
+         true, 
+         true, 
+         false);
+ 
+        // Crear el Panel del Grafico con ChartPanel
+        ChartPanel chartPanel = new ChartPanel(chart);
+        
+        return chartPanel;
+	}
+	
+	public ChartPanel ventanaRAM(){
+
+		//Con esta clase cargamos los datos que queremos reflejar 
 		DefaultPieDataset data = new DefaultPieDataset();
         for (int i = 0; i < listaAux.size(); i++) {
         	if(listaAux.get(i).getUsoMemoria()>0.1)
         		data.setValue(listaAux.get(i).getPid(),listaAux.get(i).getUsoMemoria());        
 		}
                 
-        
         // Creando el Grafico
         JFreeChart chart = ChartFactory.createPieChart(
-         "Uso de la CPU", 
+         "Uso de la Memoria RAM", 
+         data, 
+         true, 
+         true, 
+         false);
+ 
+        // Crear el Panel del Grafico con ChartPanel
+        ChartPanel chartPanel = new ChartPanel(chart);
+        
+        return chartPanel;
+	}
+	
+	public ChartPanel ventanaSWAP(){
+
+		//Con esta clase cargamos los datos que queremos reflejar 
+		DefaultPieDataset data = new DefaultPieDataset();
+        for (int i = 0; i < listaAux.size(); i++) {
+        	if(listaAux.get(i).getUsoMemoria()>0.5)
+        		data.setValue(listaAux.get(i).getPid(),listaAux.get(i).getUsoMemoria());
+		}
+                
+        // Creando el Grafico
+        JFreeChart chart = ChartFactory.createPieChart(
+         "Uso de la Memoria SWAP", 
          data, 
          true, 
          true, 
--- LectorProcesos/src/interfazGrafica/Ventana.java
+++ LectorProcesos/src/interfazGrafica/Ventana.java
@@ -2,29 +2,23 @@ package interfazGrafica;
 
 import java.awt.EventQueue;
 import java.util.List;
-
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import javax.swing.JTable;
-
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.general.DefaultPieDataset;
-
 import logica.JTablaModelo;
 import logica.Proceso;
 import logica.LanzarProceso;
-
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JLayeredPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Ventana extends JFrame {
 
@@ -76,7 +70,12 @@ public class Ventana extends JFrame {
 		JMenuItem mntmGuardarEstado = new JMenuItem("Guardar estado");
 		mnArchivo.add(mntmGuardarEstado);
 
-		JMenuItem mntmSalir = new JMenuItem("salir");
+		JMenuItem mntmSalir = new JMenuItem("Salir");
+		mntmSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		mnArchivo.add(mntmSalir);
 
 		JMenu mnAyuda = new JMenu("Ayuda");
@@ -84,6 +83,15 @@ public class Ventana extends JFrame {
 
 		JMenuItem mntmAcercaDe = new JMenuItem("Acerca de");
 		mnAyuda.add(mntmAcercaDe);
+		mntmAcercaDe.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				JOptionPane.showMessageDialog(null, "Programa creado por:\nDaniel García Merino\nDiego Jesús Torres Peinado", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+
+			}
+		});
 
 		JPanel panelNorte = new JPanel();
 		frame.getContentPane().add(panelNorte, BorderLayout.NORTH);
@@ -93,8 +101,7 @@ public class Ventana extends JFrame {
 
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setResizeWeight(.5);
-		//splitPane.setDividerLocation(0.5);
-		// splitPane.setDividerLocation();
+
 		frame.getContentPane().add(splitPane, BorderLayout.CENTER);
 
 		JScrollPane scrollPane = new JScrollPane();
@@ -107,29 +114,20 @@ public class Ventana extends JFrame {
 		splitPane.setRightComponent(panelPestañas);
 
 		JPanel panelUsoCPU = new JPanel();
-		//panelUsoCPU.setMaximumSize(getMaximumSize());
-		//panelUsoCPU.setBounds(50, 50, 500, 400);
+		JPanel panelUsoRAM = new JPanel();
+		JPanel panelUsoSWAP = new JPanel();
 		
-		//JLayeredPane usoCPU = new JLayeredPane();
 		panelPestañas.addTab("Uso CPU", null, panelUsoCPU, null);
+		panelPestañas.addTab("Uso RAM", null, panelUsoRAM, null);	
+		panelPestañas.addTab("Uso Swap", null, panelUsoSWAP, null);
 		
-		
-
 		modelo = new JTablaModelo(listadoProcesos);
-		//Creamos la base de datos del proceso
 		
 		JChartPanelPlantilla plantilla = new JChartPanelPlantilla(listadoProcesos);
 		
-        panelUsoCPU.add(plantilla.ventana());
-        //usoCPU.add(panelUsoCPU);
-		
-        
-		JLayeredPane usoRAM = new JLayeredPane();
-		panelPestañas.addTab("Uso RAM", null, usoRAM, null);
-
-		JLayeredPane usoSwap = new JLayeredPane();
-		panelPestañas.addTab("Uso Swap", null, usoSwap, null);
-		
+		panelUsoCPU.add(plantilla.ventanaCPU());
+		panelUsoRAM.add(plantilla.ventanaRAM());
+		panelUsoSWAP.add(plantilla.ventanaSWAP());
 
 	}
 }
